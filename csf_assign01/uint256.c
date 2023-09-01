e82a2c1dffec80dde9b963ff2284a0588740dc9b
Arijit Nukala <88689186+tikicode@users.noreply.github.com>
2023-09-01 05:53:22 -0400

    Add assignment 1 part 1

--- csf_assign01/uint256.c
+++ csf_assign01/uint256.c
@@ -8,8 +8,8 @@
 // Only the least-significant 32 bits are initialized directly,
 // all other bits are set to 0.
 UInt256 uint256_create_from_u32(uint32_t val) {
-  UInt256 result;
-  // TODO: implement
+  UInt256 result = {0,0,0,0,0,0,0,0}; 
+  result.data[0] = val;
   return result;
 }
 
@@ -17,15 +17,41 @@ UInt256 uint256_create_from_u32(uint32_t val) {
 // The element at index 0 is the least significant, and the element
 // at index 3 is the most significant.
 UInt256 uint256_create(const uint32_t data[8]) {
-  UInt256 result;
-  // TODO: implement
+  UInt256 result = {0,0,0,0,0,0,0,0}; 
+  for (int i = 0; i < 8; i++) {
+    result.data[i] = data[i];
+  }
   return result;
 }
 
 // Create a UInt256 value from a string of hexadecimal digits.
 UInt256 uint256_create_from_hex(const char *hex) {
-  UInt256 result;
-  // TODO: implement
+  UInt256 result = {0,0,0,0,0,0,0,0};
+  int i = 0;
+  int k = 0;
+  while (*hex) {
+    if (i < 8) {
+      if (k < 9) {
+        uint32_t j = *hex++;
+        if (j >= '0' && j <= '9') {
+          j = j - '0';
+        } else if (j >= 'A' && j <= 'F') {
+          j = j - 'A' + 10;
+        } else if (j >= 'a' && j <= 'f') {
+          j = j - 'a' + 10;
+        }
+        result.data[i] = (result.data[i] << 4) | j;
+        k++;
+      }
+      if (k == 8) {
+        i++;
+        k = 0;
+      }
+      if (i == 8) {
+        break;
+      }
+    }
+  }
   return result;
 }
 
