d7d8dea99ef4c0c0fe9cbd8aa5381a12371512a0
Arijit Nukala <88689186+tikicode@users.noreply.github.com>
2023-10-25 17:22:22 -0400

    Fix assignment

--- csf_assign03/cache.cpp
+++ csf_assign03/cache.cpp
@@ -10,19 +10,19 @@ void Cache::write(uint32_t index, uint32_t tag, int current_time) {
   auto block = sets[index].find(tag);
   if (block != sets[index].end()) {
     ++store_hits;
-    cycles += (is_write_back ? 1 : 100) + 1;
+    if (is_write_back) cycles += 1; else cycles+= 0;
     block->second.access_ts = current_time;
     block->second.is_dirty = is_write_back;
   }
   else {
     ++store_misses;
-    cycles += (is_write_allocate ? 100 : 0) + (is_write_back ? 0 : 25 * block_size);
+    if (is_write_back) cycles += 25 * block_size; // Write block main memory in 25 * n_bits cycles
+    if (is_write_allocate) cycles += 25 * block_size; // Allocate to mem
     if (is_write_allocate) {
       handle_write_action(index, tag, current_time); 
       return;
     }
   }
-    
 }
 
 // Method to handle read operations
@@ -45,7 +45,8 @@ void Cache::read(uint32_t index, uint32_t tag, int current_time) {
 void Cache::handle_write_action(uint32_t index, uint32_t tag, int current_time) {
   if (is_lru) {
     if ((int) sets[index].size() < blocks_per_set) {
-      sets[index][tag] = {false, current_time, current_time};
+      Slots slot = {false, current_time, current_time};
+      sets[index][tag] = slot;
     } else {
       uint32_t lru_tag = tag;
       int oldest_time = current_time;
@@ -58,7 +59,8 @@ void Cache::handle_write_action(uint32_t index, uint32_t tag, int current_time) 
 
       sets[index].erase(lru_tag);
 
-      sets[index][tag] = {false, current_time, current_time};
+      Slots slot = {false, current_time, current_time};
+      sets[index][tag] = slot;
     }
   } else {
     //no FIFO implementation for MS2
