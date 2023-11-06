4836d54a3583f1d4a02472b722a77b4de0232363
Taran Agarwal <tagarwa4@jh.edu>
2023-11-06 17:12:09 -0500

    incorrect

--- csf_assign04/parsort.c
+++ csf_assign04/parsort.c
@@ -72,37 +72,35 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
   size_t mid = begin + size/2;
 
   // TODO: parallelize the recursive sorting
-  pid_t pid_left = fork();
-  if (pid_left == -1) {
-    // fork failed to start a new process
-    fprintf(stderr, "fork failed to start a new process");
-  } else if (pid_left == 0) {
-    merge_sort(arr, begin, mid, threshold);
-    exit(0);
-  }
-  
-  pid_t pid_right = fork();
-  if (pid_right == -1) {
-    // fork failed to start a new process
-    fprintf(stderr, "fork failed to start a new process");
-  } else if (pid_right == 0) {
-    merge_sort(arr, mid, end, threshold);
-    exit(0);
-  }
+    pid_t pid_l = fork();
+    if (pid_l == -1) {
+      fprintf(stderr, "failed to load process");
+    } else if (pid_l == 0) {
+      merge_sort(arr, begin, mid, threshold);
+      exit(0);
+    }
 
-  int left_wstatus;
-  // blocks until the process indentified by pid_to_wait_for completes
-  pid_t left_pid = waitpid(pid_left, &left_wstatus, 0);
-  if (left_pid == -1) {
-    fprintf(stderr, "child process failure");
-  }
+    pid_t pid_r = fork();
+    if (pid_r == -1) {
+      fprintf(stderr, "failed to load process");
+    } else if (pid_r == 0) {
+      merge_sort(arr, mid, end, threshold);
+      exit(0);
+    }
 
-  int right_wstatus;
-  // blocks until the process indentified by pid_to_wait_for completes
-  pid_t right_pid = waitpid(pid_right, &right_wstatus, 0);
-  if (right_pid == -1) {
-    fprintf(stderr, "child process failure");
-  }
+    int left_wstatus;
+    // blocks until the process indentified by pid_to_wait_for completes
+    pid_t left_pid = waitpid(pid_l, &left_wstatus, 0);
+    if (left_pid == -1) {
+      fprintf(stderr, "child process failure");
+    }
+
+    int right_wstatus;
+    // blocks until the process indentified by pid_to_wait_for completes
+    pid_t right_pid = waitpid(pid_r, &right_wstatus, 0);
+    if (right_pid == -1) {
+      fprintf(stderr, "child process failure");
+    }
 
   // allocate temp array now, so we can avoid unnecessary work
   // if the malloc fails
@@ -123,6 +121,19 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
 
   // success!
 }
+
+int handle_child_work(size_t is_left, int64_t *arr, size_t begin, size_t end, size_t threshold) {
+  size_t size = end - begin;
+  size_t mid = begin + size/2;
+  if(is_left) {
+    merge_sort(arr, begin, mid, threshold);
+  } else {
+    merge_sort(arr, mid, end, threshold);
+  }
+
+  return 0;
+}
+
 
 int main(int argc, char **argv) {
   // check for correct number of command line arguments
@@ -169,14 +180,13 @@ int main(int argc, char **argv) {
   }
 
   // TODO: sort the data!
+    merge_sort(data, 0, num_elements, threshold);
 
-  merge_sort(data, 0, num_elements,  threshold);
 
   // TODO: unmap and close the file
 
-  munmap(data, file_size_bytes);
+  munmap(data, num_elements);
 
   // TODO: exit with a 0 exit code if sort was successful
-
   return 0;
 }
