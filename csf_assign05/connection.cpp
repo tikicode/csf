74760564cc62a0f464afe59cbc0af53fed0d6c27
Arijit Nukala <88689186+tikicode@users.noreply.github.com>
2023-11-29 16:42:30 -0500

    Add connection code

--- csf_assign05/connection.cpp
+++ csf_assign05/connection.cpp
@@ -13,33 +13,43 @@ Connection::Connection()
 Connection::Connection(int fd)
   : m_fd(fd)
   , m_last_result(SUCCESS) {
-  // TODO: call rio_readinitb to initialize the rio_t object
+  rio_readinitb(&m_fdbuf, fd);
 }
 
 void Connection::connect(const std::string &hostname, int port) {
-  // TODO: call open_clientfd to connect to the server
-  // TODO: call rio_readinitb to initialize the rio_t object
+  m_fd = open_clientfd(hostname.c_str(), std::to_string(port).c_str());
+  if (m_fd <= -1) fprintf(stderr, "Error: Connection failed");
+  else rio_readinitb(&m_fdbuf, m_fd);
 }
 
 Connection::~Connection() {
-  // TODO: close the socket if it is open
+  close();
 }
 
 bool Connection::is_open() const {
-  // TODO: return true if the connection is open
+  return m_fd > -1;
 }
 
 void Connection::close() {
-  // TODO: close the connection if it is open
+  if (is_open()) {
+    Close(m_fd);
+    m_fd = -1; // change status to closed
+  }
 }
 
 bool Connection::send(const Message &msg) {
-  // TODO: send a message
-  // return true if successful, false if not
-  // make sure that m_last_result is set appropriately
+  std::string message = msg.tag + ":" + msg.data + "\n";
+  ssize_t send_res = rio_writen(m_fd, message.c_str(), message.size());
+  if (send_res >= 0) {
+    m_last_result = SUCCESS;
+    return true;
+  }
+  m_last_result = EOF_OR_ERROR;
+  return false;
 }
 
 bool Connection::receive(Message &msg) {
+
   // TODO: receive a message, storing its tag and data in msg
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
